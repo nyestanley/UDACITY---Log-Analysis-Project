260316be5c9b0eafecffea55cc4d6c32543072f5
Niesha <nieshaamos@gmail.com>
2017-07-25 23:48:15 -0400

    Optimize sql statement to improve performance

--- loganalysis.py
+++ loganalysis.py
@@ -1,3 +1,4 @@
+##!/usr/bin/env python3
 import psycopg2
 import datetime
 
@@ -12,26 +13,26 @@ db = psycopg2.connect(database=databaseName)
 # Print three most popular articles of all time
 c = db.cursor()
 c.execute("select articles.title, count(log.time) as hits from articles,log" +
-          " where log.path like ('%' || articles.slug || '%') and " +
+          " where log.path =  ('/article/' || articles.slug) and " +
           " log.path!='/' group by articles.title order by hits desc limit 3;")
 
 print("Most popular three articles of all time:")
-for record in c.fetchall():
-    print(record[0] + " - " + str(record[1]) + " views")
+for title, hits in c.fetchall():
+    print(title + " - " + str(hits) + " views")
 
 
 # most populart article authors of all time
 # Make sure view v_author_articles has been created
 c.execute("select AA.name, count(log.time) from log, v_author_articles as AA" +
-          " where log.path like ('%' || AA.slug || '%') group by AA.name " +
+          " where log.path = ('/article/' || AA.slug) group by AA.name " +
           "order by count desc;")
 print("\nMost popular article authors of all time:")
 
-for record in c.fetchall():
-    print(record[0] + " - " + str(record[1]) + " views")
+for authorName, views in c.fetchall():
+    print(authorName + " - " + str(views) + " views")
 
 # On which days did more than 1% of requests lead to errors
-c.execute("select to_char(views.date, 'Month, DD YYYY'), " +
+c.execute("select to_char(views.date, 'FMMonth, DD YYYY'), " +
           "trunc((round(numOfErrors,1)/round(viewsPerDay,1))*100,2) " +
           "as percentageOfErrors from " +
           "(select errors.date, errors.numOfErrors, count(time) " +
@@ -45,7 +46,7 @@ c.execute("select to_char(views.date, 'Month, DD YYYY'), " +
           "order by percentageOfErrors desc;")
 print("\nOn which days did more than 1% of requests lead to errors?")
 
-for record in c.fetchall():
-    print(str(record[0]) + " - " + str(record[1]) + "% errors\n")
+for date, successfulStatus in c.fetchall():
+    print(str(date) + " - " + str(successfulStatus) + "% errors\n")
 
 db.close()
